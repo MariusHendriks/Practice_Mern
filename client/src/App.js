03a5fc45d637498bb171af87fc9130e6437278f0
Mark Hendriks <mark.hendriks9999@gmail.com>
2018-10-10 14:47:09 +0200

    loads of bullocks

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+//import components
 import Navbar from "./components/layouts/Navbar";
 import Landing from "./components/layouts/Landing";
 import Footer from "./components/layouts/Footer";
@@ -7,22 +11,26 @@ import Footer from "./components/layouts/Footer";
 //import auth
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+
+//import css
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
